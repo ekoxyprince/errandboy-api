c28d64207b372fedadddb0a7a4a59cae747bd650
Prince Ekejiuba <64290248+ekoxyprince@users.noreply.github.com>
2024-02-27 22:01:02 -0800

    reset password endpoint

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const Order = require('../models/order')
 const fs = require('fs')
+const bcrypt = require('bcryptjs')
+const catchAsync = require('../utilities/trycatch')
 
 exports.getRecentOrders = (req,res,next)=>{
     Order
@@ -41,3 +43,15 @@ exports.updateDetails = (req,res,next)=>{
         next(error)
     })
 }
+exports.updatePassword = catchAsync(async(req,res,next)=>{
+    const user = req.user
+    const {password,currentPassword} = req.body
+   const doMatch = await bcrypt.compare(currentPassword,user.password)
+   if(!doMatch){
+    return res.status(400).json({success:false,body:{title:'Authentication Error',status:401,data:{path:'password',value:password,location:'body',msg:'Incorrect Password.'}}})
+   }
+   const hashedPassword = await bcrypt.hash(password,12)
+   user.password = hashedPassword
+   const updatedUser = await user.save()
+   res.status(200).json({success:true,body:{title:'Response Success',status:200,data:{msg:'Updated successfully',user:updatedUser}}})
+})
